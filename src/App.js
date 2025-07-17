8cd7e2b881f307ff301abd6b88bdd99ab07a4083
Arlettesa11 <arlettesa@hotmail.com>
2025-07-16 23:39:08 -0600

    Cambiar texto principal en App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <h1>Hola desde Netlify con despliegue continuo 🚀</h1>
         </p>
         <a
           className="App-link"
